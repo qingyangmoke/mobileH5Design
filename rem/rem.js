0534cb0daceafad15b13e393839b3be4c4791e1f
qingyangmoke <qingyangmoke@qq.com>
2017-04-11 15:22:41 +0800

    simple meta query

--- rem/rem.js
+++ rem/rem.js
@@ -1,32 +1,23 @@
 (function (window, document) {
+
     function isMobile() {
         return /mobile|tablet|ip(ad|hone|od)|android/i.test(navigator.userAgent)
     }
-    // only support mobile
+
     if (!isMobile()) return
 
-    var designWidth, // 设计稿尺寸
-        rem2px, // 1rem=N像素
+    var designWidth = 640, // 设计稿尺寸
+        rem2px = 100, // 1rem=N像素
         domRoot = window,
         widthProperty = 'innerWidth',
         lastWidth = 0
 
-    designWidth = 640
-    rem2px = 100
-
-    var metaTags = document.getElementsByTagName('meta')
-
-    for (var i = 0; i < metaTags.length; i++) {
-        var e = metaTags[i]
-        if (e.getAttribute('name') == 'rem:config') {
-            var content = e.getAttribute('content')
-            designWidth = parseInt(content.match(/width=(\d+)/)[1]);
-            rem2px = parseInt(content.match(/rem2px=(\d+)/)[1]);
-            break;
-        }
+    var configMeta = document.getElementsByTagName('meta')['rem:config']
+    if (typeof configMeta !== 'undefined') {
+        var content = configMeta.getAttribute('content')
+        designWidth = parseInt(content.match(/width=(\d+)/)[1])
+        rem2px = parseInt(content.match(/rem2px=(\d+)/)[1])
     }
-
-    console.log(designWidth, rem2px)
 
     if (!(widthProperty in window)) {
         widthProperty = 'clientWidth'
@@ -43,7 +34,6 @@
 
     refreshRem()
 
-    // window.addEventListener('onorientationchange' in window ? 'orientationchange' : 'resize', refreshRem, false)
     window.addEventListener('resize', refreshRem, false)
-
+    // window.addEventListener('onorientationchange' in window ? 'orientationchange' : 'resize', refreshRem, false)
 })(window, document)
